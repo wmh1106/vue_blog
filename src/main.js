9396dae10906a1542b2b3e043a58f2b214506b9a
wmh1106 <406875886@qq.com>
2018-08-21 23:18:03 +0800

    引入 element-ui 并使用按需引入

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+import { Button, Select } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Select)
 
 Vue.config.productionTip = false
 
