00a64704f56e363062ee52f8b3e8a7dcf6108128
wmh1106 <406875886@qq.com>
2018-08-24 04:21:17 +0800

    布局-编辑、创建样式完

--- src/pages/Create/index.vue
+++ src/pages/Create/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>创建博客</div>
+  <div class="articleContent">
+    <el-form ref="form">
+      <el-form-item label="文章标题">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="文章简介">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="文章内容">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+
+      <el-form-item label="显示在首页">
+        <el-switch></el-switch>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary">立即发布</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.articleContent{
+  width: 1000px;
+  margin: 0 auto;
+}
+</style>
